07624773b1f3190131dca5c23d615b748848a875
Joshua Escano <joshuamarifrancis@gmail.com>
2024-09-24 14:15:15 +0800

    centered elements

--- index.php
+++ index.php
@@ -6,9 +6,7 @@
         <title>Pup Kitt | Login</title>
         <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-        <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
         <style>
@@ -18,8 +16,14 @@
         </style>
     </head>
     <body>
-        <div>
-            <h1>TET</h1>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4"></div>
+                <div class="col-sm-4 text-center">
+                    <h1>TEST</h1>
+                </div>
+                <div class="col-sm-4"></div>
+            </div>
         </div>
     </body>
 </html>
