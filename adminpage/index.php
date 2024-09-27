267c33411f9f3f8db723bf0f6976d9ed1911d307
Joshua Escano <joshuamarifrancis@gmail.com>
2024-09-27 16:25:01 +0800

    admin page

--- adminpage/index.php
+++ adminpage/index.php
@@ -15,56 +15,76 @@
                 background-color: #ffe8b7;
             }
             .logo{
-                width: 75px;
+                width: 70px;
             }
-            nav{
+            .nav{
                 display: flex;
                 align-items: center;
                 justify-content: space-between;
                 flex-wrap: wrap;
             }
 
-            nav ul li{
+            .nav ul li{
                 display: inline-block;
                 list-style: none;
+                list-style-position: inside;
                 margin: 10px 20px;
             }
 
-            nav ul li a{
+            .nav ul li a{
                 color: #4d4b34;
                 text-decoration: none;
-                font-size: 18px;
-                font-weight: 700;
+                font-size: 15px;
+                font-weight: 500;
                 position: relative;
             }
-            nav ul li a::after{
+            .nav ul li a::after{
                 content: '';
                 width: 0;
-                height: 3px;
+                height: 1px;
                 background: #b94013;
                 position: absolute;
-                left: 0;
-                bottom: -6px;
-                transition: 0.5s;
+                right: 0;
+                bottom: -3px;
+                transition: 0.2s;
             }
-            nav ul li a:hover::after{
+            .nav ul li a:hover::after{
                 width: 100%;
+            }
+            .profilepic{
+                width: 50px;
+                border-radius: 50%;
+            }
+            .categ{
+                border: 2px solid #b94013;
+                border-radius: 20%;
+            }
+            .categ a{
+                text-decoration: none;
+                color: #4d4b34;
             }
         </style>
     </head>
     <body>
         <div class="container">
             <div class="row mt-3">
-                <nav>
+                <nav class="nav">
                     <img src="../images/logoparent.png" class="logo">
                     <ul>
                         <li><a href="#">DASHBOARD</a></li>
                         <li><a href="#services">ORDERS</a></li>
-                        <i class="fi fi-rs-bell"></i>
+                        <li><a href="#services">PRODUCTS</a></li>
+                        <a href="#"><img class="profilepic" src="../images/profileimg.jpg" alt="profileimg"></a>
                     </ul>
                 </nav>
             </div>
-            <hr style="height: 3px">
+            <hr style="height: 2px">
+            <div class="row">
+                <h5 class="categ"><a href="#">Dog food</a></h5>
+                <h5 class="categ"><a href="#">Accessory</a></h5>
+                <h5 class="categ"><a href="#">Shampoo</a></h5>
+            </div>
         </div>
+        
     </body>
 </html>
